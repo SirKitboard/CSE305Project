604f4c42f2495f62ba01da258738753c8aa6efaa
Aditya Balwani <adibalwani@gmail.com>
2015-11-07 17:58:25 -0500

    Python test

--- env/myapp/myapp/__init__.py
+++ env/myapp/myapp/__init__.py
@@ -1,6 +1,5 @@
 from pyramid.config import Configurator
-from pyramid.config import Configurator
-from pyramid.response import Response
+
 
 def hello_world(request):
 	return Response('Hello')
@@ -10,7 +9,9 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_view(hello_world)
+    config.add_route('hello','/')
+    config.add_view(hello_world, route_name='hello')
     config.scan()
     return config.make_wsgi_app()
