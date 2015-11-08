4a884f8be50a0b206bf57e9a4f9fbafa25c1d6bb
Aditya Balwani <adibalwani@gmail.com>
2015-11-07 19:31:49 -0500

    build script test

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -2,7 +2,7 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 def hello_world(request):
-	return Response('Hello WOEIOrld')
+	return Response('Hello WEIOrld')
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
