e1957686522bf77d74e2d25dbf11737be6fd079f
Aditya Balwani <adibalwani@gmail.com>
2015-11-09 17:00:56 -0500

    Moved routes to different file

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -1,55 +1,5 @@
 from pyramid.config import Configurator
 from pyramid.response import Response
-from pyramid.view import view_config
-
-import mysql.connector
-
-
-def allItems(request):
-    cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
-    cursor = cnx.cursor()
-
-    query = ("SELECT * FROM Items ")
-
-    cursor.execute(query)
-
-    items = []
-    for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
-        items.append({
-            'id': ID,
-            'name': Name,
-            'type': Type,
-            'manufactureYear': ManufactureYear,
-            'copiesSold': CopiesSold,
-            'stock': Stock
-        })
-
-    return items
-
-
-def getItem(request):
-    itemID = request.matchdict['id']
-    cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
-    cursor = cnx.cursor()
-
-    query = ("SELECT * FROM Items WHERE ID = " + str(itemID))
-
-    cursor.execute(query)
-
-    item = None
-
-    for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
-        item = {
-            'id': ID,
-            'name': Name,
-            'type': Type,
-            'manufactureYear': ManufactureYear,
-            'copiesSold': CopiesSold,
-            'stock': Stock
-        }
-
-    return item
-
 
 def hello_world(request):
     return Response('Hello EOD')
--- tlau/env/myapp/myapp/views.py
+++ tlau/env/myapp/myapp/views.py
@@ -1,5 +1,54 @@
 from pyramid.view import view_config
 
+import mysql.connector
+
+
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'myapp'}
+
+
+def allItems(request):
+    cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
+    cursor = cnx.cursor()
+
+    query = ("SELECT * FROM Items ")
+
+    cursor.execute(query)
+
+    items = []
+    for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
+        items.append({
+            'id': ID,
+            'name': Name,
+            'type': Type,
+            'manufactureYear': ManufactureYear,
+            'copiesSold': CopiesSold,
+            'stock': Stock
+        })
+
+    return items
+
+
+def getItem(request):
+    itemID = request.matchdict['id']
+    cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
+    cursor = cnx.cursor()
+
+    query = ("SELECT * FROM Items WHERE ID = " + str(itemID))
+
+    cursor.execute(query)
+
+    item = None
+
+    for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
+        item = {
+            'id': ID,
+            'name': Name,
+            'type': Type,
+            'manufactureYear': ManufactureYear,
+            'copiesSold': CopiesSold,
+            'stock': Stock
+        }
+
+    return item
