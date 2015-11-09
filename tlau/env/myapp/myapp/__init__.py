274a554065d06528f39e57cd0a41c76dfa481ce4
Aditya Balwani <adibalwani@gmail.com>
2015-11-09 16:52:44 -0500

    wut?

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -4,31 +4,34 @@ from pyramid.view import view_config
 
 import mysql.connector
 
-cnx = None;
+cnx = None
+
 
 def allItems(request):
     cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
     cursor = cnx.cursor()
 
-    query = ("SELECT * FROM Items")
+    query = ("SELECT * FROM Items ")
 
     cursor.execute(query)
 
-    items = [];
+    items = []
     for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
         items.append({
-            'id' : ID,
-            'name' : Name,
-            'type' : Type,
-            'manufactureYear' : ManufactureYear,
-            'copiesSold' : CopiesSold,
-            'stock' : Stock
+            'id': ID,
+            'name': Name,
+            'type': Type,
+            'manufactureYear': ManufactureYear,
+            'copiesSold': CopiesSold,
+            'stock': Stock
         })
 
     return items
 
+
 def hello_world(request):
-	return Response('Hello EOD')
+    return Response('Hello EOD')
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -36,13 +39,13 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    
-    #Define all views
-    config.add_route('home', '/')
-    config.add_route('allItems','items/all')
-    config.add_route('hello','/hello')
 
-    #Define Views
+    # Define all views
+    config.add_route('home', '/')
+    config.add_route('allItems', 'items/all')
+    config.add_route('hello', '/hello')
+
+    # Define Views
     config.add_view(hello_world, route_name='hello')
     config.add_view(allItems, route_name='allItems', renderer='json')
     config.scan()
