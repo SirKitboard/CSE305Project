274729550360c1a2a6073b7be992947e5280f621
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 03:33:38 -0500

    added login route;

---
+++ tlau/env/myapp/myapp/UserAPI.py
@@ -0,0 +1,105 @@
+from pyramid.view import view_config
+from pyramid.response import Response
+
+import pyramid.httpexceptions as exc
+import crypt
+
+import mysql.connector
+
+
+@view_config(route_name='login', renderer='json')
+def login(request):
+    requiredKeys = ['username', 'password', 'type']
+    postVars = request.POST
+    acceptedKeys = []
+
+    for key in requiredKeys:
+        if(key in postVars):
+            acceptedKeys.append(postVars[key])
+        else:
+            raise exc.HTTPBadRequest()
+
+    salt = 'qwerty'
+    cryptedPassword = crypt.crypt(acceptedKeys[1], salt)
+    print(cryptedPassword)
+    try:
+        query = ("SELECT Type FROM Users WHERE Username=%s AND Password=%s")
+
+        cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
+        cursor = cnx.cursor()
+
+        cursor.execute(query, (acceptedKeys[0], cryptedPassword))
+        row = cursor.fetchone()
+
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        return Response("Something went wrong: {}".format(err))
+
+    print row
+    print acceptedKeys
+    if(row[0] == int(acceptedKeys[2])):
+        raise exc.HTTPOk()
+    else:
+        raise exc.HTTPUnauthorized()
+
+
+# @view_config(route_name='allItems', renderer='json')
+# def allItems(request):
+#     cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
+#     cursor = cnx.cursor()
+#
+#     query = ("SELECT * FROM Items ")
+#
+#     cursor.execute(query)
+#
+#     items = []
+#     for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
+#         items.append({
+#             'id': ID,
+#             'name': Name,
+#             'type': Type,
+#             'manufactureYear': ManufactureYear,
+#             'copiesSold': CopiesSold,
+#             'stock': Stock
+#         })
+#
+#     cursor.close()
+#     cnx.close()
+#
+#     if(len(items) == 0):
+#         raise exc.HTTPNoContent()
+#
+#     return items
+
+
+# @view_config(route_name='getItem', renderer='json')
+# def getItem(request):
+#     itemID = request.matchdict['id']
+#     cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
+#     cursor = cnx.cursor()
+#
+#     query = ("SELECT * FROM Items WHERE ID = " + str(itemID))
+#
+#     cursor.execute(query)
+#
+#     item = None
+#
+#     for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
+#         item = {
+#             'id': ID,
+#             'name': Name,
+#             'type': Type,
+#             'manufactureYear': ManufactureYear,
+#             'copiesSold': CopiesSold,
+#             'stock': Stock
+#         }
+#
+#     if(item is None):
+#         raise exc.HTTPNoContent()
+#
+#     cursor.close()
+#     cnx.commit()
+#     cnx.close()
+#
+#     return item
--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -10,12 +10,17 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    # Define all views
+    # Define all routes
     config.add_route('home', '/')
+
+    # Item Routes
     config.add_route('allItems', 'api/items')
     config.add_route('getItem', 'api/items/{id}')
     config.add_route('addItem', 'api/items', request_method='POST')
     config.add_route('hello', '/hello')
-    
+
+    # User Routes
+    config.add_route('login', 'api/login')
+
     config.scan()
     return config.make_wsgi_app()
