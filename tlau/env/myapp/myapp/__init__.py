043ea70ade1b9b83a5ec3337d51fc5297969a47f
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 01:36:13 -0500

    Completed Items API

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -1,34 +1,7 @@
 from pyramid.config import Configurator
 from pyramid.response import Response
-from pyramid.view import view_config
+import views
 
-import mysql.connector
-
-cnx = None;
-
-def allItems(request):
-    cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
-    cursor = cnx.cursor()
-
-    query = ("SELECT * FROM Items")
-
-    cursor.execute(query)
-
-    items = [];
-    for (ID, Name, Type, ManufactureYear, CopiesSold, Stock) in cursor:
-        items.append({
-            'id' : ID,
-            'name' : Name,
-            'type' : Type,
-            'manufactureYear' : ManufactureYear,
-            'copiesSold' : CopiesSold,
-            'stock' : Stock
-        })
-
-    return items
-
-def hello_world(request):
-	return Response('Hello EOD')
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -36,14 +9,13 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    
-    #Define all views
-    config.add_route('home', '/')
-    config.add_route('allItems','items/all')
-    config.add_route('hello','/hello')
 
-    #Define Views
-    config.add_view(hello_world, route_name='hello')
-    config.add_view(allItems, route_name='allItems', renderer='json')
+    # Define all views
+    config.add_route('home', '/')
+    config.add_route('allItems', 'items/all')
+    config.add_route('getItem', 'items/{id}')
+    config.add_route('addItem', 'items', request_method='POST')
+    config.add_route('hello', '/hello')
+
     config.scan()
     return config.make_wsgi_app()
--- tlau/env/myapp/myapp/views.py
+++ tlau/env/myapp/myapp/views.py
@@ -1,5 +1,8 @@
 from pyramid.view import view_config
+from pyramid.response import Response
+
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'myapp'}
+
