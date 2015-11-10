d4e9581117341846df8c8eeeb469c195934123ed
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 02:39:11 -0500

    Added Login Route

--- tlau/env/myapp/myapp/ItemAPI.py
+++ tlau/env/myapp/myapp/ItemAPI.py
@@ -41,9 +41,9 @@ def getItem(request):
     cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
     cursor = cnx.cursor()
 
-    query = "SELECT * FROM Items WHERE ID = %s"
+    query = "SELECT * FROM Items WHERE ID = %s" + str(itemID)
 
-    cursor.execute(query, tuple(itemID))
+    cursor.execute(query)
 
     item = None
 
