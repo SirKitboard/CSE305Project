a6b2f8d77bf60119467278a1abb0df022c1b9781
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 02:21:38 -0500

    Added Login Route

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -10,12 +10,17 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    # Define all views
+    # Define all routes
     config.add_route('home', '/')
-    config.add_route('allItems', 'items/all')
-    config.add_route('getItem', 'items/{id}')
-    config.add_route('addItem', 'items', request_method='POST')
+
+    # Item Routes
+    config.add_route('allItems', 'api/items')
+    config.add_route('getItem', 'api/items/{id}')
+    config.add_route('addItem', 'api/items')
     config.add_route('hello', '/hello')
+
+    # User Routes
+    config.add_route('login', 'api/login')
 
     config.scan()
     return config.make_wsgi_app()
