1812c342af4d2c145ea73c34800c812963de5730
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 15:27:02 -0500

    Added Session Management, Login and Logout

--- tlau/env/myapp/myapp/UserAPI.py
+++ tlau/env/myapp/myapp/UserAPI.py
@@ -12,6 +12,10 @@ def login(request):
     requiredKeys = ['username', 'password', 'type']
     postVars = request.POST
     acceptedKeys = []
+    session = request.session
+
+    if('currentUser' in session):
+        return session['currentUser']
 
     for key in requiredKeys:
         if(key in postVars):
@@ -23,28 +27,96 @@ def login(request):
     cryptedPassword = crypt.crypt(acceptedKeys[1], salt)
     print(cryptedPassword)
     try:
-        query = ("SELECT Type FROM Users WHERE Username=%s AND Password=%s")
+        query = ("SELECT Type, ID FROM Users WHERE Username=%s AND Password=%s")
 
         cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
-        cursor = cnx.cursor()
+        cursor1 = cnx.cursor()
+        cursor2 = cnx.cursor(dictionary=True)
 
-        cursor.execute(query, (acceptedKeys[0], cryptedPassword))
-        row = cursor.fetchone()
+        cursor1.execute(query, (acceptedKeys[0], cryptedPassword))
+        row = cursor1.fetchone()
 
-        cursor.close()
+        if(row[0] != int(acceptedKeys[2])):
+            cursor1.close()
+            cursor2.close(dictionary=True)
+            cnx.close()
+            raise exc.HTTPUnauthorized()
+
+        if(row[0] == 0):
+            query = ("SELECT * FROM Customers WHERE ID = %s")
+            cursor2.execute(query, tuple(str(row[1])))
+
+            for row in cursor2:
+                print(row)
+                user = {
+                    'type': 0,
+                    'id': row['ID'],
+                    'name': row['FirstName'] + row['LastName'],
+                    'address': row['Address'],
+                    'city': row['City'],
+                    'state': row['State'],
+                    'zipCode': row['ZipCode'],
+                    'telephone': row['Telephone'],
+                    'email': row['Email'],
+                    'itemsSold': row['ItemsSold'],
+                    'itemsPurchased': row['ItemsPurchased'],
+                    'rating': row['Rating']
+                }
+                session['currentUser'] = user
+        else:
+            query = ("SELECT * FROM Employees WHERE ID = %s")
+            cursor2.execute(query, (row[1]))
+
+            for row in cursor2:
+                user = {
+                    'type': 1,
+                    'id': row['ID'],
+                    'name': row['FirstName'] + row['LastName'],
+                    'address': row['Address'],
+                    'city': row['City'],
+                    'state': row['State'],
+                    'zipCode': row['ZipCode'],
+                    'telephone': row['Telephone'],
+                    'startDate': row['startDate'],
+                    'hourlyRate': row['hourlyRate'],
+                    'employeeType': row['type'],
+                }
+                session['currentUser'] = user
+
+        cursor2.close()
+        cursor1.close()
         cnx.close()
     except mysql.connector.Error as err:
         return Response("Something went wrong: {}".format(err))
+        cursor2.close()
+        cursor1.close()
+        cnx.close()
 
-    print row
-    print acceptedKeys
-    if(row[0] == int(acceptedKeys[2])):
+    return session['currentUser']
+    raise exc.HTTPOk()
+
+
+@view_config(route_name='logout')
+def logout(request):
+    session = request.session
+
+    if 'currentUser' not in session:
         raise exc.HTTPOk()
+    else:
+        session.pop('currentUser')
+        raise exc.HTTPOk()
+
+
+@view_config(route_name='currentUser', renderer='json')
+def currentUser(request):
+    session = request.session
+    if 'currentUser' in session:
+        return session['currentUser']
     else:
         raise exc.HTTPUnauthorized()
 
 
-# @view_config(route_name='allItems', renderer='json')
+# @view_config(route_name='allItems''], renderer='json')
 # def allItems(request):
 #     cnx = mysql.connector.connect(user='root', password='SmolkaSucks69', host='127.0.0.1', database='305')
 #     cursor = cnx.cursor()
--- tlau/env/myapp/myapp/views.py
+++ tlau/env/myapp/myapp/views.py
@@ -6,3 +6,17 @@ from pyramid.response import Response
 def my_view(request):
     return {'project': 'myapp'}
 
+
+# @view_config(route_name='sessionTest', renderer='json')
+# def sessionTest(request):
+#     session = request.session
+#     if 'access_token' in session:
+#         # session['access_token'] = session['access_token'] + 1
+#         return {
+#             'access_token': session['access_token']
+#         }
+#     else:
+#         session['access_token'] = 1
+#         return {
+#             'access_token': session['access_token']
+#         }
