e04727766bb9a5677ef31aeae002796333a8b4ee
Aditya Balwani <adibalwani@gmail.com>
2015-11-10 16:31:04 -0500

    Added current user route

--- tlau/env/myapp/myapp/__init__.py
+++ tlau/env/myapp/myapp/__init__.py
@@ -9,7 +9,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', '', cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.set_session_factory(my_session_factory)
 
     # Define all routes
